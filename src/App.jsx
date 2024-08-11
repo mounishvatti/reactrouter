af9e88063daef61720ea851c38a33b30cee1d2be
mounishvatti <mounishvatti2002@gmail.com>
2024-08-11 19:07:43 +0530

    added home, about and contact page routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
 
   return (
     <>
-    <h1 className='text-3xl font-bold underline'>Vite + React</h1>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'about',
+        element: <About />
+      },
+      {
+        path: 'contact',
+        element: <Contact />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router = {router} />
   </StrictMode>,
 )
